884803579238ca6a5579288cc4c72db07ab1aaed
Josh Mervine <jmervine@yp.com>
2014-02-22 11:42:57 -0800

    Refactoring tests, to be more readable.

--- test/test.js
+++ test/test.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
-var test    = require('tape');
+var tape    = require('tape');
 var http    = require('http-debug').http;
-
 var path    = require('path');
 var fs      = require('fs');
 var qs      = require('querystring');
 var format  = require('util').format;
 var request = require('request-lite');
-var config  = require(path.resolve(__dirname, 'test.json'));
 
 // supress output
 
@@ -30,99 +28,70 @@ process.env.TEST_CONFIG = path.resolve(__dirname, 'test.json');
 process.env.TEST_PORT = 10888;
 var server = require('../');
 
+tape('git fish', function (group) {
 
-// TODO: This needs a serious look. It started out as a good idea
-// and then ballooned.
-function testRequest(t, opts, callback) {
-    if (typeof opts === 'function') {
-        callback = opts;
-        opts = {};
-    }
-
-    // default to valid passing options
-    opts.code = opts.code || 200;
-    opts.endpoint = opts.endpoint || '/test1';
-    opts.token = opts.token || config.token;
-    opts.method = opts.method || 'POST';
-    opts.data = opts.data || post_data;
-    opts.form = opts.form || false;
-
-    var url = format('http://localhost:%s%s?token=%s',
-                        process.env.TEST_PORT,
-                        opts.endpoint,
-                        opts.token);
-
-    function requestCallback(error, response, _) {
-        t.error(error, format('%s %s with %s should not error',
-                      opts.method, opts.endpoint, (opts.form ? 'form' : 'json')));
-
-        t.ok(response, format('%s %s with %s should have response',
-                      opts.method, opts.endpoint, (opts.form ? 'form' : 'json')));
-
-        t.equal(response.statusCode, opts.code,
-            format('%s %s with %s should have %s response',
-                      opts.method, opts.endpoint, (opts.form ? 'form' : 'json'), opts.code));
-
-        if (typeof callback === 'function') {
-            callback();
-        }
-   }
-
-    if (opts.method === 'POST') {
-        if (opts.form) {
-            request.post(url, requestCallback).form({ payload : JSON.stringify(opts.data) });
-        } else {
-            request.post(url, requestCallback).json(opts.data);
-        }
-    } else {
-        request[opts.method.toLowerCase()](url, requestCallback);
-    }
-}
-
-test('git fish', function (g) {
-
-    g.test('test valid json', function (t) {
-        t.plan(4);
-        cleanup();
-        testRequest(t, function() {
+    group.test('test valid json', function (test) {
+        request.post('http://localhost:10888/test1?token=go-fish', { json: post_data }, function(err, res, body) {
+            test.error(err, 'should not error')
+            test.ok(res, 'should return response');
+            test.equal(res.statusCode, 200, 'should return 200');
             setTimeout(function () {
                 // give it a second
-                t.ok(fs.existsSync('/tmp/.git.fish.test.out'), 'should run script');
+                test.ok(fs.existsSync('/tmp/.git.fish.test.out'), 'should run script');
                 cleanup();
+                test.end();
             }, 200);
         });
     });
 
-    g.test('test valid form', function (t) {
-        t.plan(4);
-        cleanup();
-        testRequest(t, { form: true }, function() {
+    group.test('test valid form', function (test) {
+        request.post('http://localhost:10888/test1?token=go-fish', { form: { payload: JSON.stringify(post_data) } }, function(err, res, body) {
+            test.error(err, 'should not error')
+            test.ok(res, 'should return response');
+            test.equal(res.statusCode, 200, 'should return 200');
             setTimeout(function () {
                 // give it a second
-                t.ok(fs.existsSync('/tmp/.git.fish.test.out'), 'should run script');
+                test.ok(fs.existsSync('/tmp/.git.fish.test.out'), 'should run script');
                 cleanup();
+                test.end();
             }, 200);
         });
     });
 
-    g.test('bad token', function (t) {
-        t.plan(3);
-        testRequest(t, { code: 403, token: 'bad' });
+    group.test('bad method', function (test) {
+        request.post('http://localhost:10888/test1?token=badtoken', { json: post_data }, function(err, res, body) {
+            test.error(err, 'should not error')
+            test.ok(res, 'should return response');
+            test.equal(res.statusCode, 403, 'should return 403');
+            test.end();
+        });
     });
 
-    g.test('bad method', function (t) {
-        t.plan(3);
-        testRequest(t, { code: 403, method: 'GET' });
+    group.test('bad method', function (test) {
+        request.put('http://localhost:10888/test1?token=go-fish', { json: post_data }, function(err, res, body) {
+            test.error(err, 'should not error')
+            test.ok(res, 'should return response');
+            test.equal(res.statusCode, 403, 'should return 403');
+            test.end();
+        });
     });
 
-    g.test('bad data', function (t) {
-        t.plan(3);
-        testRequest(t, { code: 500, data: {} });
+    group.test('bad data', function (test) {
+        request.post('http://localhost:10888/test1?token=go-fish', { json: { bad: 'data' } }, function(err, res, body) {
+            test.error(err, 'should not error')
+            test.ok(res, 'should return response');
+            test.equal(res.statusCode, 500, 'should return 500');
+            test.end();
+        });
     });
 
-    g.test('bad path', function (t) {
-        t.plan(3);
-        testRequest(t, { code: 404, endpoint: '/bad' });
+    group.test('bad path', function (test) {
+        request.post('http://localhost:10888/bad?token=go-fish', { json: post_data }, function(err, res, body) {
+            test.error(err, 'should not error')
+            test.ok(res, 'should return response');
+            test.equal(res.statusCode, 404, 'should return 500');
+            test.end();
+        });
     });
 
     // Give tests 2 seconds to complete and then exit,
